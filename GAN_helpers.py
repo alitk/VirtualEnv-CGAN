39739efa9aff93632dadef3868f1d8f1b6144e6e
Taleb <alitk@vt.edu>
2020-04-07 11:35:15 -0400

    start feeding custom data into dataloader

--- GAN_helpers.py
+++ GAN_helpers.py
@@ -20,36 +20,12 @@ import torch
 
 os.makedirs("images", exist_ok=True)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
-parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
-parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
-parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
-parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
-parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
-parser.add_argument("--img_size", type=int, default=28, help="size of each image dimension")
-parser.add_argument("--channels", type=int, default=1, help="number of image channels")
-parser.add_argument("--sample_interval", type=int, default=400, help="interval betwen image samples")
-opt = parser.parse_args()
-print(opt)
-
 img_shape = (opt.channels, opt.img_size, opt.img_size)
 
 cuda = True if torch.cuda.is_available() else False
 
-arguments = {
-    n_epochs: 200, #number of epochs of training
-    batch_size: 64, #size of the batches
-    lr: 0.0002, #adam: learning rate")
-    b1: 0.5, #adam: decay of first order momentum of gradient")
-    b2: 0.999, # adam: decay of first order momentum of gradient")
-    img_size: 28, # 
-    latent_dim: 100, # "dimensionality of the latent space")
-    channels: 1,
-    sample_interval: 400, # interval betwen image samples
-}
 class Generator(nn.Module):
-    def __init__(self):
+    def __init__(self,latent_dim):
         super(Generator, self).__init__()
 
         def block(in_feat, out_feat, normalize=True):
@@ -60,7 +36,7 @@ class Generator(nn.Module):
             return layers
 
         self.model = nn.Sequential(
-            *block(opt.latent_dim, 128, normalize=False),
+            *block(latent_dim, 128, normalize=False),
             *block(128, 256),
             *block(256, 512),
             *block(512, 1024),
@@ -98,7 +74,7 @@ class Discriminator(nn.Module):
 
 
 class GAN():
-    def __init__(self, n_epochs = 200, batch_size= 64, lr= 0.0002, b1= 0.5, 
+    def __init__(self, input_data, n_epochs = 200, batch_size= 64, lr= 0.0002, b1= 0.5, 
                 b2= 0.999, img_size= 28, latent_dim= 100, channels= 1, 
                 sample_interval= 400):
         self.n_epochs = n_epochs
@@ -129,24 +105,18 @@ class GAN():
     def train
 
 # Configure data loader
-os.makedirs("../../data/mnist", exist_ok=True)
-dataloader = torch.utils.data.DataLoader(
-    datasets.MNIST(
-        "../../data/mnist",
-        train=True,
-        download=True,
-        transform=transforms.Compose(
-            [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
-        ),
-    ),
-    batch_size=opt.batch_size,
-    shuffle=True,
-)
+# os.makedirs("../../data/mnist", exist_ok=True)
+
+    dataloader = torch.utils.data.DataLoader(
+        input_data,
+        batch_size=self.batch_size,
+        shuffle=True,
+    )
 
 
 
-
-Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+'''
+    Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
 # ----------
 #  Training
@@ -202,3 +172,5 @@ for epoch in range(opt.n_epochs):
         batches_done = epoch * len(dataloader) + i
         if batches_done % opt.sample_interval == 0:
             save_image(gen_imgs.data[:25], "images/%d.png" % batches_done, nrow=5, normalize=True)
+
+'''
